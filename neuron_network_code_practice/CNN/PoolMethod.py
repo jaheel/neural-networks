4e9ffac2c4cdec7f13fbd3b1858684d16b4a0c54
alex <xufanxin86@gmail.com>
2021-01-31 16:42:48 +0800

    feature: pool layer

--- neuron_network_code_practice/CNN/PoolMethod.py
+++ neuron_network_code_practice/CNN/PoolMethod.py
@@ -1,127 +1,224 @@
 import numpy as np
 
 class PoolMethod(object):
-    def __init__(self, in_data, filter_size=2, stride=2):
-        """
+    def __init__(self, in_data, filter_size = (2,2), strides = (1,1), pool_name ='MAX'):
+        """tensor pool
         
-        池化初始化
 
         Parameters
         ----------
-        in_data : {array-like} of shape(n_col, n_row) origin data 2D 
+        in_data : {array-like, tensor(3-dim)} of shape (in_data_col, in_data_row, in_data_channel) 
         
-        filter_size : (1_number)
+        filter_size : {tuple-like, vector_2} of shape (filter_col, filter_row)
         
-        stride : (1_number)
+        strides : {tuple-like, vector_2} of shape (col_stride, row_stride)
+
+        pool_name : {string-like, scalar}  value of {'MAX'(default), 'AVG'}
         """
         self.in_data = in_data
         self.filter_size = filter_size
-        self.stride = stride
+        self.strides = strides
+        self.pool_name = pool_name
+        self.channel = in_data.shape[2]
+
+        self.output_shape = ( int((in_data.shape[0] - filter_size[0]) / strides[0]) + 1, 
+                              int((in_data.shape[1] - filter_size[1]) / strides[1]) + 1,
+                              self.channel)
 
     def __max_pool_single(self, in_data):
-        """
+        """single max pool scalar
 
-        单次最大池化
+
 
         Parameters
         ----------
-        in_data : {array-like} of shape(filter_size, filter_size) 
+        in_data : {array-like, matrix} of shape(filter_col, filter_row) 
         
         Returns
         -------
-        result : (1_number) the max result
+        result : {float-like, scalar} the max result
         """
-        if in_data.shape[0] != self.filter_size or in_data.shape[1] != self.filter_size:
+        if in_data.shape[0] != self.filter_size[0] or in_data.shape[1] != self.filter_size[1]:
             return None
         
         result = 0
         
-        for x_index in range(self.filter_size):
-            for y_index in range(self.filter_size):
+        for x_index in range(self.filter_size[0]):
+            for y_index in range(self.filter_size[1]):
                 result = np.maximum(result, in_data[x_index][y_index])
             
         return result
     
     def __average_pool_single(self, in_data):
-        """
+        """single average pool scalar
 
-        单次平均池化
+        
 
         Parameters
         ----------
-        in_data : {array-like} of shape(filter_size, filter_size) 
+        in_data : {array-like, matrix} of shape(filter_col, filter_row) 
         
         Returns
         -------
-        result : (1_number) the average result
+        result : {float-like, scalar} the average result
         """
-        if in_data.shape[0] != self.filter_size or in_data.shape[1] != self.filter_size:
+        if in_data.shape[0] != self.filter_size[0] or in_data.shape[1] != self.filter_size[1]:
             return None
         
         result = 0
         
-        for x_index in range(self.filter_size):
-            for y_index in range(self.filter_size):
+        for x_index in range(self.filter_size[0]):
+            for y_index in range(self.filter_size[1]):
                 result += in_data[x_index][y_index]
             
-        return result/(self.filter_size ** 2)
+        return result/(self.filter_size[0] * self.filter_size[1])
         
     
-    def max_pool(self):
-        """
-
-        最大池化
+    def __max_pool(self, in_data):
+        """max pool matrix
 
         C : side_length
 
         C_output = (C_input - C_kernel)/strides + 1
 
+        Parameters
+        ----------
+        in_data : {array-like, matrix} of shape (in_data_col, in_data_row)
+        
         Returns
         -------
-        result : {array-like} of shape((in_data.shape[0]-filter_size)/stride+1, (in_data.shape[1]-filter_size)/stride+1 )
+        result : {array-like, matrix} of shape( (in_data_col - filter_col) / stride_col + 1, (in_data_row - filter_row) / stride_row + 1)
         """
         
         result = []
-        filter_number = int(self.filter_size-1/2)
+        filter_number_h = self.filter_size[0] - 1
+        filter_number_w = self.filter_size[1] - 1
 
-        for y_index in range(0, self.in_data.shape[0] - filter_number, self.stride):
+        for y_index in range(0, in_data.shape[0] - filter_number_h, self.strides[0]):
             result_x = []
 
-            for x_index in range(0, self.in_data.shape[1] - filter_number, self.stride):
-                max_pool_in_data = self.in_data[y_index : y_index + self.filter_size, x_index : x_index + self.filter_size]
+            for x_index in range(0, in_data.shape[1] - filter_number_w, self.strides[1]):
+                max_pool_in_data = in_data[y_index : y_index + self.filter_size[0], x_index : x_index + self.filter_size[1]]
                 result_x.append(self.__max_pool_single(in_data = max_pool_in_data))
             
             result.append(result_x)
         
         return np.array(result)
 
-    def average_pool(self):
-        """
-
-        平均池化
+    def __average_pool(self, in_data):
+        """average pool matrix
 
         C : side_length
 
         C_output = (C_input - C_kernel)/strides + 1
 
+        Parameters
+        ----------
+        in_data : {array-like, matrix} of shape (in_data_col, in_data_row)
+        
         Returns
         -------
-        result : {array-like} of shape( (in_data.shape[0]-filter_size)/stride+1, (in_data.shape[1]-filter_size)/stride+1 )
+        result : {array-like, matrix} of shape( (in_data_col - filter_col) / stride_col + 1, (in_data_row - filter_row) / stride_row + 1)
         """
         
         result = []
-        filter_number = int(self.filter_size-1/2)
+        filter_number_h = self.filter_size[0] - 1
+        filter_number_w = self.filter_size[1] - 1
 
-        for y_index in range(0, self.in_data.shape[0] - filter_number, self.stride):
+        for y_index in range(0, in_data.shape[0] - filter_number_h, self.strides[0]):
             result_x = []
 
-            for x_index in range(0, self.in_data.shape[1] - filter_number, self.stride):
-                average_pool_in_data = self.in_data[y_index : y_index + self.filter_size, x_index : x_index + self.filter_size]
+            for x_index in range(0, in_data.shape[1] - filter_number_w, self.strides[1]):
+                average_pool_in_data = in_data[y_index : y_index + self.filter_size[0], x_index : x_index + self.filter_size[1]]
                 result_x.append(self.__average_pool_single(in_data = average_pool_in_data))
             
             result.append(result_x)
         
         return np.array(result)
+
+    def __find_max_index(self, in_data, find_scalar):
+        """find max index
+
+        Parameters
+        ----------
+        in_data : {array-like, matrix} of shape (filter_col, filter_row)
+
+        find_scalar : {float-like, scalar}
+
+        Returns
+        -------
+        scalar_index : {array-like, vector_2} of value [h_index, w_index]
+        """
+        for h_index in range(in_data.shape[0]):
+            for w_index in range(in_data.shape[1]):
+                if in_data[h_index][w_index] == find_scalar:
+                    return np.array([h_index, w_index]) 
+        
+
+    def forward_pass(self):
+        """pool method forward
+
+
+        Returns
+        -------
+        result : {array-like, tensor(3-dim)} of shape (out_data_col, out_data_row, channel)
+        """
+
+        result = np.zeros(self.output_shape)
+
+        if self.pool_name == 'MAX':
+            for channel_index in range(self.channel):
+                result[:, :, channel_index] = self.__max_pool(in_data = self.in_data[:, :, channel_index])
+
+        elif self.pool_name == 'AVG':
+            for channel_index in range(self.channel):
+                result[:, :, channel_index] = self.__average_pool(in_data = self.in_data[:, :, channel_index])
+
+        else:
+            ValueError("pool method name is wrong")
+
+        return result
+
+    def backward_pass(self, error, out_data):
+        """pool method backward
+
+        Parameters
+        ----------
+        error : {array-like, tensor(3-dim)} of shape (out_data_col, out_data_row, channel)
+
+        out_data : {array-like, tensor(3-dim)} of shape (out_data_col, out_data_row, channel)
+
+        Returns
+        -------
+        error_pre : {array-like, tensor(3-dim)} of shape (in_data_col, in_data_row, channel)
+                    previous layer error tensor
+        """
+        result = np.zeros(self.in_data.shape)
+        
+        if self.pool_name == 'MAX':
+            for channel_index in range(self.channel):
+                for o_h_index in range(out_data.shape[0]):
+                    for o_w_index in range(out_data.shape[1]):
+                        in_h_index = o_h_index * self.strides[0]
+                        in_w_index = o_w_index * self.strides[1]
+                        scalar_find_index = self.__find_max_index(in_data = self.in_data[in_h_index : in_h_index + self.filter_size[0], in_w_index : in_w_index + self.filter_size[1], channel_index], find_scalar = out_data[o_h_index, o_w_index, channel_index])
+                        result[in_h_index + scalar_find_index[0], in_w_index + scalar_find_index[1], channel_index] = error[o_h_index, o_w_index, channel_index]
+
+        elif self.pool_name == 'AVG':
+            for channel_index in range(self.channel):
+                for o_h_index in range(out_data.shape[0]):
+                    for o_w_index in range(out_data.shape[1]):
+                        in_h_index = o_h_index * self.strides[0]
+                        in_w_index = o_w_index * self.strides[1]
+                        result[in_h_index : in_h_index + self.filter_size[0], in_w_index : in_w_index + self.filter_size[1], channel_index] = error[o_h_index, o_w_index, channel_index] / (self.filter_size[0] * self.filter_size[1])
+        
+        else:
+            ValueError("pool method name is wrong")
+        
+        return result
+
+
+
 
     
 
@@ -160,8 +257,10 @@ class PoolMethod(object):
 #     ]
 # )
 
-# test_max_pool = MaxPool(in_data=test_data, filter_size=3, stride=2)
-# print(test_max_pool.max_pool())
+# test_data = np.random.randint(10, size=(5, 5, 3))
+# print(test_data)
+# test_max_pool = PoolMethod(in_data = test_data, filter_size = (2, 2), strides = (2, 2), pool_name = 'AVG')
+# print(test_max_pool.fit())
 # -------------------
 
 # --------------
